6c806d3b27f332e63dbd1e2a13ea7b63f958cf92
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-18 02:14:58 +0530

    Python Intro

---
+++ string_example.py
@@ -0,0 +1,11 @@
+raw = r'this\t\n and that'
+
+# this\t\n and that
+print(raw)
+
+multi = """It was the best of times.
+ It was the worst of times."""
+
+# It was the best of times.
+#   It was the worst of times.
+print(multi)
