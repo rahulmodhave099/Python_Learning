ed89ccf3596c94444ad30d367d7a7ec2adb3d7f2
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-18 02:01:34 +0530

    Python Introduction

---
+++ input_example.py
@@ -0,0 +1,5 @@
+movieName = input("Enter Movie Name : ")
+noTickets = int(input("Enter number of tickets : "))
+collection = float(input("Enter the overall income generated bu movie : "))
+
+print("We have booked ",noTickets," tickets of Movie ",movieName," which has generated overall income of ",collection," Crores")
