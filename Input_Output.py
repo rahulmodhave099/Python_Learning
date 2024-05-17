adebb11e3cda36e25b3a33d6cac7ec7efac77799
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-18 02:07:42 +0530

    Python Introduction

---
+++ Input_Output.py
@@ -0,0 +1,15 @@
+x = 10
+y = 20
+
+print(x + y)
+
+a = input("Enter value for a : ")
+b = input("Enter value for b : ")
+
+print(a + b)
+
+p = int(input("Enter value for p : "))
+q = int(input("Enter value for q : "))
+
+print(p + q)
+
