d697d10004526e807348e24cc34daa1bcb48a958
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-21 01:13:00 +0530

    DataType Numeric

---
+++ DataTypes/Numeric_data_type.py
@@ -0,0 +1,34 @@
+#Numeric Types
+
+# 1. int type
+empID = 10
+
+print(empID)        #10
+print(type(empID))  #<class 'int'>
+
+jersyNo = 7
+print(jersyNo)      #7
+print(type(jersyNo))    #<class 'int'>
+
+
+# 2. float type
+
+price = 75.50
+print(price)            #75.50
+print(type(price))      #<class 'float'>
+
+
+data = 45.777777777888888999999999999
+print(data)             #45.777777777888886
+print(type(data))       #<class 'float'>
+
+# 3. complex type
+
+complexData = 10 + 5j
+print(complexData)      #10 + 5j
+print(type(complexData))    #<class 'complex'>
+
+
+
+
+
