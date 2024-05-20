2b15c1480d15e87a356f6fde4a5399215a155fc3
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-21 01:10:23 +0530

    DataType bool

---
+++ DataTypes/boolean_data_type.py
@@ -0,0 +1,19 @@
+# Boolean Type
+
+data1 = True
+print(data1)        #True
+print(type(data1))  #<class 'bool'>
+
+data2 = False
+
+print(data2)        #False
+print(type(data2))  #<class 'bool'>
+
+x = 15
+y = 20
+
+print(x == y)       #False
+
+ans = data1 + data2
+
+print(ans)          #1
