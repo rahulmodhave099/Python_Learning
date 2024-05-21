835ec2f862b4a7501b98aff685c57f859a498631
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-22 01:37:53 +0530

    Operator Logical

---
+++ Operators/Logical_operator.py
@@ -0,0 +1,25 @@
+# Logical Operator
+# and , or , not
+#For boolean values
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
+
+#For numerical values
+
+a = 10
+b = 20
+
+print(a and b)
+print(a or b)
+print(not a)
+
+a = 0
+b = 20
+
+print(a and b)
+print(a or b)
+print(not a)
