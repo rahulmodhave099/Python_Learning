a091567e4d1b586ba3b4083cca88c61517deaa21
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-22 00:21:47 +0530

    Operator Arithmatic

---
+++ Operators/Arithmatic_operator.py
@@ -0,0 +1,12 @@
+
+# Binary Operator
+
+x = 2
+y = 5
+
+print(x + y)            # 7
+print(x - y)            # -3
+print(x * y)            # 10
+print(x / y)            # 0.4
+print(x // y)            # 0
+print(x ** y)            # 32
