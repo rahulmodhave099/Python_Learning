685aab0d82edd6a743aed85466615e1acc020ec8
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-22 00:40:35 +0530

    Operator Bitwise

---
+++ Operators/BitWise_operator.py
@@ -0,0 +1,47 @@
+
+# Bitwise and &
+
+x = 10                              # 0 0 0 0 1 0 1 0
+y = 5                               # 0 0 0 0 0 1 0 1
+
+print(x & y)                        # 0 0 0 0 0 0 0 0
+
+a = 7
+b = 8
+
+print(a & b)
+
+p = 12
+q = 7
+
+print(p & q)
+
+# Bitwise or |
+
+x = 5
+y = 8
+
+print(x | y)
+
+# Bitwise xor ^
+
+a = 14
+b = 18
+
+print(a ^ b)
+
+# Bitwise Leftshift (<<) and Rightshift (>>)4
+
+a = 12
+
+print(a << 2)
+print(a >> 2)
+
+# Bitwise Not (~)
+
+a = 4
+
+print(~ a)              # ans must be -(a+1)
+
+
+
