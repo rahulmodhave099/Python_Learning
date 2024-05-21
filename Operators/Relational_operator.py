1a4755ec15d1568089003b85b13c34ae8de499fb
Rahul Modhave <rahulmodhave099@gmail.com>
2024-05-22 01:39:49 +0530

    Relational Operator

---
+++ Operators/Relational_operator.py
@@ -0,0 +1,13 @@
+# Relational Operator
+
+x = 10
+y = 20
+
+print(x < y)             #True
+print(x > y)             #False
+print(x <= y)            #True
+print(x >= y)            #False
+print(x == y)            #False
+print(x != y)            #True
+
+
