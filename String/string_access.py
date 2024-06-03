a36395cda0ab6cf658f4dcdaf14111c949a77de7
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-04 00:00:38 +0530

    string access

---
+++ String/string_access.py
@@ -0,0 +1,30 @@
+str1 = "Core2Web"
+
+print(str1)
+
+print(str1[3])                  #e
+print(str1[7])                  #b
+print(str1[-2])                 #e
+print(str1[-5])                 #e
+
+# slicing
+
+print(str1[::2])                        #Cr2e
+print(str1[2:5:])                       #re2
+print(str1[-2:-5:-1])                   #eW2
+print(str1[-3::-1])                     #W2eroC
+
+str1 = "core2web"
+str2 = "core2web"
+
+print(str1 == str2)
+
+str3 = "Core2web"
+str4 = "core2web"
+
+print(str3 < str4)
+
+str5 = "core2web"
+str6 = "Core2web"
+
+print(str5 < str6)
