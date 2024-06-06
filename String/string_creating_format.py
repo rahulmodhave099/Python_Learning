4ea0d208cab1839832255489d205a4e3df22351a
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-06 23:49:18 +0530

    string format

---
+++ String/string_creating_format.py
@@ -0,0 +1,42 @@
+# String Creating types
+# type 1
+
+str1 = 'core2web'
+print(str1)
+print(type(str1))
+print(len(str1))
+
+# type 2
+
+str2 = "incubator"
+print(str2)
+print(type(str2))
+print(len(str2))
+
+# type 3
+
+str3 = '''
+Biencaps
+system
+pvt
+ltd
+'''
+print(str3)
+print(type(str3))
+print(len(str3))
+
+str4 = 'Welcome to "IIT" Narhe'
+print(str4)
+
+str5 = "Welcome to \tCore2Web \n Pune"
+print(str5)
+
+str6 = r"Welcome to \tCore2Web \n Pune"
+print(str6)
+
+str7 = "\u0906\u0908"
+print(str7)
+
+str8 = '\u0915 \u0916 \u0917'
+print(str8)
+
