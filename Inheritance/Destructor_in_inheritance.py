ad27e9a8da395b58e4be28bea46762655ac225a3
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-08 18:25:09 +0530

    Destructor in Inhertance : calling parent constructor inside child constructor

--- Inheritance/Destructor_in_inheritance.py
+++ Inheritance/Destructor_in_inheritance.py
@@ -23,7 +23,6 @@ print(obj.dispData)
 obj.dispData()
 
 Parent.dispParent()
-'''
 
 # Destructor
 
@@ -56,3 +55,30 @@ print("#############################################")
 obj3 = Parent()
 obj3 = Parent()
 print("Code samplay")
+'''
+class Parent1:
+    def __init__(self):
+        print("In parent Constructor")
+        self.x = 10
+        self.y = 20
+
+    def dispParent(self):
+        print(self.x)
+        print(self.y)
+
+class Child(Parent1):
+
+    def __init__(self):
+        print("In child Constructor")
+        # super().__init__()
+        self.x = 30
+        self.y = 40
+        super().__init__()
+
+    def dispChild(self):
+        print(self.x)
+        print(self.y)
+
+obj = Child()
+obj.dispParent()
+obj.dispChild()
