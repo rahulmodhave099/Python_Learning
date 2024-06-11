d998e8ee58e2dde094a491d67ce554477743317a
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-12 00:36:58 +0530

    MRO multiple inheritance example

--- Inheritance/MRO_multiple_inheritance.py
+++ Inheritance/MRO_multiple_inheritance.py
@@ -1,10 +1,39 @@
-class Data:
-    pass
+class Boss:
 
-obj1 = Data()
-obj2 = Data()
+    def report(self):
+        print("Boss : Report")
 
-print(id(obj1))
-print(id(obj2))
 
-print(obj1 == obj2)
+class Manager1(Boss):
+
+    def report(self):
+        print("Manager 1 : Report")
+
+class Manager2(Boss):
+
+    def report(self):
+        print("Manager 2 : Report")
+
+class Manager3(Boss):
+
+    def report(self):
+        print("Manager 3 : Report")
+
+class TeamLead1(Manager1,Manager3):
+
+    def report(self):
+        print("TeamLead 1 : Report")
+
+class TeamLead2(Manager2,Manager3):
+
+    def report(self):
+        print("TeamLead 2 : Report")
+
+class Developer(TeamLead1,TeamLead2):
+
+    def report(self):
+        print("Developer : Report")
+
+print(Developer.__mro__)
+
+print(Developer.mro())
