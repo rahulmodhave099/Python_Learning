bf6ab33818c4e8150c0a9278811322c5eb5053c6
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-22 17:08:17 +0530

    exceptions types and examples

--- Exception handling/Exceptions_handlinng.py
+++ Exception handling/Exceptions_handlinng.py
@@ -13,7 +13,7 @@ def fun():
 
 fun()
 
-# type error
+# type error  : it occurres when it deos not match with the given entity.
 
 def fun(a,b):
     print(a)
@@ -24,10 +24,29 @@ fun(10,20)
 fun(10)
 print("End Code")
 
+# OR
 
 def fun(a, b):
     print(a+b)
 
 fun(10,"Rahul")
 print("End Code")
+
+# Attribute error : it occurres when it deos not find any entity/method in class.
+
+class Demo:
+
+    def fun(self):
+        print("In Fun")
+
+Demo().gun()
+
+ # Name error  : it occurres when it deos not find any entity/method in outside class/global space.
+
+class Demo:
+
+    def fun(self):
+        print("In Fun")
+
+fun()
 
