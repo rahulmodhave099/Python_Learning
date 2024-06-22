2365c459fd6d049a5810a5f4f1ae08097aaa9b04
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-23 00:23:30 +0530

    exceptions types and examples

---
+++ Exception handling/exception1a.py
@@ -0,0 +1,18 @@
+
+class Demo:
+
+    def fun(self):
+        print("In fun")
+
+    def gun(self):
+        print("In gun")
+
+
+class Memo:
+
+    def run(self):
+        print("In run")
+
+    def mun(self):
+        print("In mun")
+
