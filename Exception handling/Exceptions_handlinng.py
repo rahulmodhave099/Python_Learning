3e093d89f9d2fd0409b82926df3a53d3331c6db7
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-22 16:58:55 +0530

    exceptions types and examples

--- Exception handling/Exceptions_handlinng.py
+++ Exception handling/Exceptions_handlinng.py
@@ -12,3 +12,22 @@ def fun():
         print("Hello")
 
 fun()
+
+# type error
+
+def fun(a,b):
+    print(a)
+    print(b)
+
+print("Start Code")
+fun(10,20)
+fun(10)
+print("End Code")
+
+
+def fun(a, b):
+    print(a+b)
+
+fun(10,"Rahul")
+print("End Code")
+
