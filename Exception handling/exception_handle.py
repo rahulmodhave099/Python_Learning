49be9f7fba3e2d414e87d3f3419a31ae0e111a28
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-23 14:36:36 +0530

    exception handle example

--- Exception handling/exception_handle.py
+++ Exception handling/exception_handle.py
@@ -1,4 +1,4 @@
-
+'''
 print("Start Code")
 
 empid = 10
@@ -11,4 +11,22 @@ except:
     print("Exception handled")
 
 print("End code")
+
+
+print("Start code")
+try:
+    num1 = int(input("Enter value for num1 : "))
+    num2 = int(input("Enter value for num2 : "))
+
+except ValueError as err:
+    print("Exception Handled")
+
+try :
+    print(num1 + num2)
+except NameError as err:
+    print("Exception Handled")
+
+print("End code")
+'''
+
 
