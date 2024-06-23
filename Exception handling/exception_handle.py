6996dc09cfa0c8d769d56ef52b29bae617fd0024
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-23 14:36:52 +0530

    exception handle example

--- Exception handling/exception_handle.py
+++ Exception handling/exception_handle.py
@@ -29,4 +29,15 @@ except NameError as err:
 print("End code")
 '''
 
+list1 = [10,"kanha",20,"Rahul",30]
 
+try:
+    print(list1[int(input("Value for index : "))])
+    print(list1[int(input("Value for index : "))])
+
+    add = list1[0] + list1[3]
+
+except (ValueError,IndexError,TypeError) as err:
+    print(err)
+
+print("End code")
