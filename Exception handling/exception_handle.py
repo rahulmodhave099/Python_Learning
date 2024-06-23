c18fc5398fed6a38ae6dfcdd3d8986989d89ffa9
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-23 16:08:59 +0530

    exception handle example

--- Exception handling/exception_handle.py
+++ Exception handling/exception_handle.py
@@ -27,7 +27,7 @@ except NameError as err:
     print("Exception Handled")
 
 print("End code")
-'''
+
 
 list1 = [10,"kanha",20,"Rahul",30]
 
@@ -35,9 +35,38 @@ try:
     print(list1[int(input("Value for index : "))])
     print(list1[int(input("Value for index : "))])
 
-    add = list1[0] + list1[3]
+    add = list1[0] + list1[2]
 
 except (ValueError,IndexError,TypeError) as err:
     print(err)
+else:
+    print(add)
 
 print("End code")
+
+'''
+
+def voting(name,age):
+
+    if age<18:
+        raise ValueError("Not Eligible for voting")
+    else:
+        print("Thanks for voting")
+
+print("Start Code")
+
+name = input("Enter your name : ")
+
+try:
+    age = int(input("Enter your age : "))
+
+except ValueError:
+    print("Proper integer value de ")
+
+try:
+    voting(name,age)
+
+except ValueError as err:
+    print(err)
+
+print("End Code")
