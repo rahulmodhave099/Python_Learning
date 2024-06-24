460922334ad9d2490b3c2726d47e91fa87406946
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-24 16:19:33 +0530

    file handling open example

---
+++ File Handling/file_handling.py
@@ -0,0 +1,12 @@
+# File Handling
+# 2 types of files
+# 1. Text files : txt,pdf
+# 2. binary files : jpg,mp3,mp4
+
+# Opening and closing a file
+# open("filename","filemode")   where filemode is task/reason of file for opening e.g read,write
+
+fileobj = open("core2web.txt")
+
+print(type(fileobj))
+
