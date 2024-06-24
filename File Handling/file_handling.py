ed796ccaaf34b8b5d1ea021770a2429ee1e9e2b6
Rahul Modhave <rahulmodhave099@gmail.com>
2024-06-25 00:54:11 +0530

    file handling read types

--- File Handling/file_handling.py
+++ File Handling/file_handling.py
@@ -5,7 +5,7 @@
 
 # Opening and closing a file
 # open("filename","filemode")   where filemode is task/reason of file for opening e.g read,write
-
+'''
 fileobj = open("core2web.txt")
 
 print(type(fileobj))
@@ -15,3 +15,39 @@ print(type(fileobj))
 fileobj = open("core2web.txt","r")
 
 print(fileobj.read())
+
+f = open("incubator.txt","r")
+
+print(f.read())
+print(f.read())
+
+f.seek(0)
+
+print(f.read())
+'''
+
+# reading and writing into a file
+
+f1 = open("core2web.txt","r+")
+
+print(f1.readable())
+print("###################################################")
+
+# 3 types of reading files : read,readline,readlines
+
+print(f1.tell())
+print("###################################################")
+
+print(f1.read())
+print("###################################################")
+
+print(f1.tell())
+print("###################################################")
+
+f1.seek(0)
+
+print(f1.readline())
+print("###################################################")
+
+f1.seek(0)
+print(f1.readlines())
