cadbe01e06ea32fd04f5a35d683340de9f01adf8
Rahul Modhave <rahulmodhave099@gmail.com>
2024-07-05 01:25:13 +0530

    python database connectivity sqlite insert data into table

--- PDBC/sqlite.py
+++ PDBC/sqlite.py
@@ -6,6 +6,7 @@ conn = sqlite3.connect("IPL.db")
 
 cursorObj = conn.cursor()
 
+"""
 createTable = '''
     create table Players(
     jerno INTEGER(5) NOT NULL PRIMARY KEY,
@@ -16,5 +17,27 @@ createTable = '''
 '''
 
 cursorObj.execute(createTable)
+
+
+# insert into table
+
+player1 = '''insert into Players values(18,"Virat","RCB","M","India")'''
+player2 = '''insert into Players values(33,"Warner","DC","M","Australia")'''
+player3 = '''insert into Players values(41,"Mandhana","RCB","F","India")'''
+player4 = '''insert into Players values(45,"Rohit","MI","M","India")'''
+player5 = '''insert into Players values(12,"Perry","MI","F","England")'''
+player6 = '''insert into Players values(93,"Bumrah","MI","M","India")'''
+
+cursorObj.execute(player1)
+cursorObj.execute(player2)
+cursorObj.execute(player3)
+cursorObj.execute(player4)
+cursorObj.execute(player5)
+cursorObj.execute(player6)
+
+conn.commit()
+"""
+
+
 
 conn.close()
